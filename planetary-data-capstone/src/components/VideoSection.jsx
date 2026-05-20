98f8a799119e5cc3925bdf65d6a08b68c4502aa8
seltimk <seltimkatzingbale@gmail.com>
2026-05-20 15:05:37 +0100

    Completed video section

--- planetary-data-capstone/src/components/VideoSection.jsx
+++ planetary-data-capstone/src/components/VideoSection.jsx
@@ -1,7 +1,28 @@
+import "./VideoSection.css";
+
 function VideoSection() {
   return (
-    <section>
-      <h2>Video Section here</h2>
+    <section className="vid-sect-container">
+
+      <div className="video-section">
+        <h3>How Planetary Data Helps Us Understand Space</h3>
+
+        <p>
+          Planetary science goes beyond images. Comparing<a href="#" target="_blank"> mass</a>,<a href="#" target="_blank"> diameter</a>,
+          <a href="#" target="_blank"> gravity</a>, and <a href="#" target="_blank"> density</a> helps us understand planets.
+        </p>
+      </div>
+
+      <div className="video1">
+        <video
+          src="https://www.pexels.com/download/video/2695085/"
+          controls
+          autoPlay
+          muted
+          loop
+        />
+      </div>
+
     </section>
   );
 }
